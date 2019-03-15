80c6d35856993d2ead8dd8246a87807cf3b571ea
Francisco <franbarra100@gmail.com>
2019-03-14 23:24:00 -0300

    Removed blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
-import Layout from '../components/Layout'
 import Link from 'next/link'
+import Layout from '../components/Layout'
+import Carousel from '../components/Carousel'
 
 
-const PostLink = (props) => (
-    <li>
-      <Link href={`/post?title=${props.title}`}>
-        <a>{props.title}</a>
-      </Link>
-    </li>
-  )
-  
+
   export default () => (
     <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink title="Hello Next.js"/>
-        <PostLink title="Learn Next.js is awesome"/>
-        <PostLink title="Deploy apps with Zeit"/>
-      </ul>
+        <Carousel />
     </Layout>
   )

65ba91a315861dd0bed1adaa1f09b92700816b02
Francisco <franbarra100@gmail.com>
2019-03-14 23:21:38 -0300

    Created Header as a separate component

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,20 @@
 import Navbar from './Navbar'
-import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+
 
 const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD',
+  margin: 0,
 }
 
-
 const Layout = (props) => (
-  <div class="layoutStyle">
-    <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="Francisco Barraguirre" />
-        <title>Julieta Nogueira</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/lux/bootstrap.min.css" />
-    </Head>
+  <div style={layoutStyle}>
+    <Header />
     <Navbar />
-    <div className="container-fluid">
+    <div style={layoutStyle}>
       {props.children}
     </div>
+    <Footer />
   </div>
 )
 

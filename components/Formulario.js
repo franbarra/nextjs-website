ff95bf4daae34a6a03851826cf05b27f2be99365
Francisco <franbarra100@gmail.com>
2019-03-14 23:21:15 -0300

    Removed inline-options form field

--- components/Formulario.js
+++ components/Formulario.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 const Formulario = () => (
 
-<div className="container">
+<div className="container" style={{marginTop: 20}}>
     <form>
     <fieldset>
         <legend>Enviame tu consulta</legend>
         <div className="form-group row">
-        <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
+            <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
         <div className="col-sm-10">
             <input type="text" readonly="" className="form-control-plaintext" id="staticEmail" value="email@example.com" />
         </div>
@@ -18,28 +18,23 @@ const Formulario = () => (
             <input type="text" className="form-control" placeholder="Ingresar nombre" id="inputDefault" />
         </div>
         <div className="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar e-mail" />
-        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresar e-mail" />
+            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
         </div>
         <div className="form-group">
             <label className="col-form-label" for="inputDefault">Asunto</label>
             <input type="text" className="form-control" id="inputDefault" />
         </div>
-        <div className="form-group">
-        <select className="custom-select">
-        <option selected="">Sobre que trata tu consulta?</option>
-        <option value="1">Precio</option>
-        <option value="2">Redes Sociales</option>
-        <option value="3">Contratacion</option>
-        <option value="4">Otros</option>
-        </select>
-    </div>
         <div class="form-group">
-        <label for="exampleTextarea">Dejame tu mensaje</label>
-        <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
+            <label for="exampleTextarea">Dejame tu mensaje</label>
+            <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">Submit</button>
     </fieldset>
     </form>
 </div>
